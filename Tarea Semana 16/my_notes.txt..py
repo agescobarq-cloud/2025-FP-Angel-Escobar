54093e330a1cf8b783ecfbf43ed4efb365e34222
ESCOBAR QUEZADA ANGEL GABRIEL <ag.escobarq@uea.edu.ec>
2025-10-01 23:28:24 -0600

    Tarea semana 16

--- Tarea Semana 16/my_notes.txt..py
+++ Tarea Semana 16/my_notes.txt..py
@@ -2,8 +2,8 @@
 with open('my_notes.txt', 'w') as file:
     # Escribir tres líneas de notas personales
     file.write("Nota 1: Recordar terminar la tarea de Python.\n")
-    file.write("Nota 2: Comprar víveres el fin de semana.\n")
-    file.write("Nota 3: Llamar a mamá para el domingo.\n")
+    file.write("Nota 2: Comprar comida el fin de semana.\n")
+    file.write("Nota 3: Llamar a mi novia para el domingo.\n")
 
 # Abrir y leer el archivo my_notes.txt
 with open('my_notes.txt', 'r') as file:
